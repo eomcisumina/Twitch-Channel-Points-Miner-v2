0ccf27c18b24956fd1ea5655ffe69bcbb91f05fd
eomcisumina <79391447+eomcisumina@users.noreply.github.com>
2021-05-22 07:33:11 +0800

    revert

--- TwitchChannelPointsMiner/classes/WebSocketsPool.py
+++ TwitchChannelPointsMiner/classes/WebSocketsPool.py
@@ -265,7 +265,7 @@ class WebSocketsPool:
                                     start_after = event.closing_bet_after(current_tmsp)
 
                                     place_bet_thread = Timer(
-                                        max(10,start_after-90),
+                                        start_after,
                                         ws.twitch.make_predictions,
                                         (ws.events_predictions[event_id],),
                                     )
